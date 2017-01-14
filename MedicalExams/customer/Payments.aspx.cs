374e6a34b2af878c0fa905dde18fcb42f545badc
Anna <anna.mocko96@gmail.com>
2017-01-14 16:05:16 +0000

    Add Payments

---
+++ MedicalExams/customer/Payments.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Payments : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void gridviewPayments_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (gridviewPayments.SelectedIndex >= 0)
+        {
+            btPay.Enabled = true;
+        }
+        else
+        {
+            btPay.Enabled = false;
+        }
+    }
+}
